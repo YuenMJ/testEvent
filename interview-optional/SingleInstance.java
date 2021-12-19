ef6f14d6a67879dd8e6fa2a1606e21c5b0a137fb
MJ <yuen.mj@gmail.com>
2021-12-19 23:42:40 +0800

    task 4

--- interview-optional/SingleInstance.java
+++ interview-optional/SingleInstance.java
@@ -4,22 +4,47 @@ package com.strands.spf;
  * <p>
  * Single instance in a multi threading environment
  * </p>
- * 
+ *
  * @author strands
- * 
+ *
  */
 public class SingleInstance {
 
-  public SingleInstance() {
-    // TODO Auto-generated constructor stub
-  }
+    private static SingleInstance instance;
+    private static int staticNum = 0;
+    private int nonStaticNum = 0;
 
-  /**
-   * @param args
-   */
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    public SingleInstance() {
+    }
 
-  }
+    public SingleInstance getInstance() {
+        if (instance == null) {
+            instance = new SingleInstance();
+        }
+        return instance;
+    }
+
+    public void addStaticAndNonStaticNum() {
+        staticNum++;
+        nonStaticNum++;
+    }
+
+    public void printBothNum() {
+        System.out.println("Static Num      : " + staticNum);
+        System.out.println("Non Static Num  : " + nonStaticNum);
+    }
+
+    public static void main(String[] args) {
+        SingleInstance A = new SingleInstance().getInstance();
+        A.addStaticAndNonStaticNum();
+        A.printBothNum();
+        SingleInstance B = new SingleInstance().getInstance();
+        B.addStaticAndNonStaticNum();
+        B.printBothNum();
+        SingleInstance C = new SingleInstance().getInstance();
+        C.addStaticAndNonStaticNum();
+        C.printBothNum();
+
+    }
 
 }
